9196f09764026db690cf5e11ddc506c3de48a081
jacklaing123@hotmail.co.uk <jacklaing123@hotmail.co.uk>
2025-07-17 11:58:45 +0100

    Confirmed script operational

--- Wk3d4/script.js
+++ Wk3d4/script.js
@@ -2,6 +2,7 @@
 // Search
 const searchDiv = document.querySelector('#searchDiv')
 const search = document.querySelector('#search')
+const searchBtn = document.querySelector('#searchBtn')
 
 // Info
 const infoMain = document.querySelector('#info')
@@ -19,14 +20,14 @@ const tempMin = document.querySelector('#tempMin')
 const tempMax = document.querySelector('#tempMax')
 const pressure = document.querySelector('#pressure')
 const humidity = document.querySelector('#humidity')
-
+let city = 'London'
 // Funcs
 const getWeatherData = async ()=>{
     const api_key = 'b02f35934fb0cba82950cbbf2907fa55'
     let city = 'London'
     const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api_key + '&units=metric'
 
-    fetch(url) 
+    return fetch(url) 
     .then((response) =>{ 
         if (response.ok) { return response.json(); } 
     }) 
@@ -40,3 +41,22 @@ const getWeatherData = async ()=>{
     });
 }
 
+const displayWeather = async ()=>{
+    console.log("Displaying weather...");
+
+    try{
+        const weatherData = await getWeatherData();
+
+        // Main Display
+        cityDisplay.textContent = city;
+        weatherMain.textContent = weatherData.weather[0].main;
+
+    }finally{
+
+    }
+}
+
+
+searchBtn.addEventListener('click', ()=>{
+    displayWeather();
+})
