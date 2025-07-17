f8209b16eaad744722546ed7212ae290fb8beb63
jacklaing123@hotmail.co.uk <jacklaing123@hotmail.co.uk>
2025-07-17 11:29:37 +0100

    Added getWeatherData()

--- Wk3d4/script.js
+++ Wk3d4/script.js
@@ -1,3 +1,23 @@
-const api_key = 'b02f35934fb0cba82950cbbf2907fa55'
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`
+// Element definitions
+
+
+// Funcs
+const getWeatherData = async ()=>{
+    const api_key = 'b02f35934fb0cba82950cbbf2907fa55'
+    let city = 'London'
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + api_key + '&units=metric'
+
+    fetch(url) 
+    .then((response) =>{ 
+        if (response.ok) { return response.json(); } 
+    }) 
+    .then((data) =>{ 
+        if (!data) return; 
+        console.log(`Successfully retrieved weather data for: ${city}`);
+        return data;
+    }) 
+    .catch((error) =>{ 
+        console.log(`ERROR: ${error}`);
+    });
+}
 
